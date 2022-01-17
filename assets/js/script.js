babbe315a6caf7c380a0feb377c54a58d7865a5d
Gravity <tevoned@gmail.com>
2022-01-16 19:39:00 -0500

    submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,6 +46,127 @@
         const answerContainer = answerContainer[questionNumber]; const selector = 'input[name=question${questionNumber}]:checked';
         const userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
+        // if answer is correct then....
+        if(userAnswer === currentQuestion.correctAnswer){
+
+        // add to the number of correct answers
+            
+            numCorrect++;
+
+        // color green if answer is correct 
+
+            answerContainer[questionNumber].style.color = 'lightgreen';
+        }
+
+        // color is red if question is not answerd or wrong 
+        else{
+
+            answerContainer[questionNumber].style.color = 'red';
+        }
+    });
+
+    // show the number of correct answer
+    resultContainer.innerHTML ='${numCorrect} out of ${myQuestions.lenght}';
+}
+
+function showSlide(n) {
+
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if(currentSlide === 0){previousButton.style.display = 'none';
+}
+else{
+ previousButton.style.display = 'inline-black';
+}
+if(currentSlide === slides.lenght-1){
+    nextButton.style.display = 'none';
+    submitButton.style.display = 'inline-block';
+}
+else{
+    nextButton,style.display = 'inline-block'; submitButton.style.display = 'none';
+}
+}
+
+function showNextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// variables
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitbutton = document.getElementById('submit');
+const myQuestions = [
+    {
+        question: "Arrays in javaScript can be used to store?",
+        answers: {
+            a: "number and strings",
+            b: "other arrays",
+            c: "booleans",
+            d: "all of the above"
+        },
+        correctAnswer: "d"
+    },
+    { 
+
+        question: "Commonly used data types Do Not Include",
+        answers: {
+            a: "strings",
+            b: "booleans",
+            c: "alerts",
+            d: "numbers"
+        },
+        correctAnswer: "c"
+    },
+    {
+
+        question: "A very useful tool used during development and debugging for printing content to the debugger is",
+        answers: {
+            a: "javaScript",
+            b: "termianl/bash",
+            c: "for loops",
+            d: "console.log"
+        },
+        correctAnswer: "d"
+    },
+];
+
+// begin
+buildQuiz();
+
+// pagination
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const slides = document.querySelectorAll(".Slide");
+let currentSlide = 0;
+
+// show the first slide
+showSlide(currentSlide);
+
+// Event listeners
+    submitbutton.addEventListner('click',showResult);
+
+    previousButton.addEventListener("click", showPreviousSlide);
+
+    nextButton.addEventListener("click", showNextSlide);
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
             function getTimeRemaining(endtime) {
                 const total = Date.parse(endtime) - Date.parse(new Date());
                 const seconds = Math.floor((total / 1000) % 60);
